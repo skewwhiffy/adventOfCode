d332a117f87e2c77557ae2b15e4bc77bf1e7773f
Kenny Hung <kenny.hung@just-eat.com>
2017-12-06 12:35:50 +0000

    The rest of day 1

---
+++ Advent.Of.Code.Core/Day1.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace Advent.Of.Code.Core
+{
+    public class Day1
+    {
+        public int Sum(string rawData, bool halfWayRound)
+        {
+            var rawList = rawData
+                .Select(c => int.Parse(c.ToString()))
+                .ToList();
+            var sum = 0;
+            var offset = halfWayRound ? rawList.Count / 2 : 1;
+            for (var i = 0; i < rawList.Count; i++)
+            {
+                var current = rawList[i];
+                var next = rawList[(i + offset) % rawList.Count];
+                if (current == next)
+                {
+                    sum += current;
+                }
+            }
+            return sum;
+        }
+    }
+}
